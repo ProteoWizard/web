29cfb03b2606b8dbde9ee709b485c48dfa45a68f
Matt Chambers <matt.chambers42@gmail.com>
2021-07-02 13:53:22 -0400

    Add Google Analytics hits for downloads

--- js/main.js
+++ js/main.js
@@ -141,6 +141,13 @@ function download() {
             var teamCityDownloadURL = matches[0];
             var hrefMatches = teamCityDownloadURL.match(/builds\/id:(\d+)\/artifacts\/content\/(.*)/);
             var downloadURL = `https://proteowizard-teamcity-artifacts.s3.us-west-2.amazonaws.com/ProteoWizard/${downloadTypeString}/${hrefMatches[1]}/${hrefMatches[2]}`;
+
+            // Send Google Analytics hit for downloaded file
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})
+              (window,document,'script','//www.google-analytics.com/analytics.js','ga2');
+            ga2("create", "UA-54926454-1", "auto");
+            ga2("send", {"hitType": "pageview", "page": "/download/" + hrefMatches[2]});
+
             if(email) {
                 writeEmailToFile(email, function() {
                     window.location = downloadURL;
